8f5ce4aa9c02a9ca1042775da3ce8f892c2c9de7
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-10-27 23:59:38 -0600

    Add files via upload

--- ZillowSearch.py
+++ ZillowSearch.py
@@ -2,24 +2,19 @@ from dataclasses import dataclass
 import requests
 import json
 
-@dataclass
+@dataclass(frozen=True)
 class ZillowSearch():
-    def __init__(self, headers: dict):
-        self.headers = headers
-    
-    def findAllHouseData(self, state: str, pages: int) -> list[dict]:
+    def findAllHouseData(self, state: str, pages: int, headers: dict) -> list[dict]:
         houseArray = []
         
         for page in range(pages):
             url = 'https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState={"pagination":{"currentPage":' + str(page + 1) +'},"mapBounds":{"west":-109.060256,"east":-102.040878,"south":36.992424,"north":41.003444},"regionSelection":[{"regionId":10,"regionType":2}],"isMapVisible":false,"filterState":{"isAllHomes":{"value":true},"sortSelection":{"value":"days"}},"isListVisible":true}&wants={"cat1":["listResults"],"cat2":["total"]}&requestId=2'
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, headers=headers)
             houseArray.extend(json.loads(response.content)["cat1"]["searchResults"]["listResults"])
         
         return houseArray
     
     def findAllHousePrices(self, state: str=None) -> list[int]:
-        # response = requests.get('https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState={"pagination":{"currentPage":3},"mapBounds":{"west":-109.060256,"east":-102.040878,"south":36.992424,"north":41.003444},"regionSelection":[{"regionId":10,"regionType":2}],"isMapVisible":false,"filterState":{"isAllHomes":{"value":true},"sortSelection":{"value":"days"}},"isListVisible":true}&wants={"cat1":["listResults"],"cat2":["total"]}&requestId=2', headers=self.headers)
-        #                          https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState={"pagination":{"currentPage":2},"usersSearchTerm":"Denver, CO","mapBounds":{"west":-105.109927,"east":-104.600296,"south":39.614431,"north":39.914247},"regionSelection":[{"regionId":11093,"regionType":6}],"isMapVisible":false,"filterState":{"sortSelection":{"value":"days"},"isAllHomes":{"value":true}},"isListVisible":true}&wants={"cat1":["listResults"],"cat2":["total"]}&requestId=2
         raise NotImplementedError()
 
 def main(headers):
